9df5a61e52db9858795070a0b980aa1e9913e27e
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2023-01-01 20:13:12 +0530

    #1

---
+++ java_Basics/Conditions.java
@@ -0,0 +1,61 @@
+import java.net.SocketPermission;
+import java.util.Scanner;
+
+import javax.sound.midi.Synthesizer;
+
+public class Conditions {
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int age = sc.nextInt();
+
+    //     if(age > 18){
+    //         System.out.println("Adult");
+    //     }else{
+    //         System.out.println("not adult");
+    //     }
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int x  = sc.nextInt();
+    // if(x % 2 ==0){
+    //     System.out.println("even");
+    // }else{
+    //     System.out.println("odd");
+    // }
+    // }
+
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int a  = sc.nextInt();
+    //     int b  = sc.nextInt();
+    // if(a == b){
+    //     System.out.println("bith are esqual");
+    // }
+    // else{
+    //     if(a > b){
+    //         System.out.println("a is greater");
+    //     }else{
+    //         System.out.println("a is less than b");s
+    //     }
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int button  = sc.nextInt();
+    
+    //    switch(button) {
+    //     case 1 : System.out.println("hello");
+    //     break;
+    //     case 2: System.out.println("Namaste");
+    //     break;
+    //     case 3: System.out.println("benjaro");
+    //     break;
+    //     default :System.out.println("invalid button");
+    //    }
+    // }
+
+
+}
