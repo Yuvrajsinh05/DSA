fa15179dea641574f0738c96fdcfa0b2741510c8
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2023-01-02 16:08:57 +0530

    #2 loops

---
+++ java_Basics/Loops.java
@@ -0,0 +1,30 @@
+public class Loops {
+ public static void main(String args[]) {
+
+    // for(int counter = 0; counter<3; counter=counter+1){
+    //     System.out.println("hello world");
+    // }
+
+    // for(int counter = 0; counter<11; counter++){
+    //     System.out.print(counter+ " ");
+    // }
+    
+    // int i =0;
+    // while(i<10){
+    //     System.out.println(i);
+    //     i = i + 1; // i++
+    // }
+
+    // int i = 0;
+    // do{
+    //     System.out.println(i);
+    //     i++;
+    // }while(i<11);
+ }   
+}
+
+
+// for(initialization;condition;updation){
+//     // write
+// }
+
