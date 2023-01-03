415db9651871c8325d4b22ef24688a992dce1718
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2023-01-03 22:54:41 +0530

    #3 patterns

--- java_Basics/Patterns.java
+++ java_Basics/Patterns.java
@@ -37,15 +37,28 @@ public class Patterns {
         //  }
 
 
-         for(int i=n; i>=1 ; i--){
-            //innerLoop
-            for(int j=1; j<=i;j++){
-                System.out.print("*");
+        //  for(int i=n; i>=1 ; i--){
+        //     //innerLoop
+        //     for(int j=1; j<=i;j++){
+        //         System.out.print("*");
              
+        //     }
+        //     System.out.println();
+        //  }
+
+    //outer loop
+        for(int i=1;i<=n;i++){
+            //inner loop -> space print
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+
+            // inner loop -> start print
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
             }
             System.out.println();
-         }
-
+        }
     }
 }
  
