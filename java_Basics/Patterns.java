000648f22015a122cf48371257edb6d1ba3dc1c1
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2023-01-04 20:10:12 +0530

    #4 patterns

--- java_Basics/Patterns.java
+++ java_Basics/Patterns.java
@@ -76,14 +76,27 @@ public class Patterns {
     //     System.out.println();
     // }
 
-    int num = 1 ;
-    for(int i =1;i<=n;i++){
-        for(int j=1 ; j<=i;j++){
-            System.out.print(num + " ");
-            num++;
+    // int num = 1 ;
+    // for(int i =1;i<=n;i++){
+    //     for(int j=1 ; j<=i;j++){
+    //         System.out.print(num + " ");
+    //         num++;
+    //     }
+    //     System.out.println();
+    // }
+    
+    for(int i = 1; i<=n;i++){
+        for(int j=1; j<=i;j++){
+            int sum = i+j;
+            if(sum%2 ==0){
+                System.out.print("1 ");
+            }else{
+                System.out.print("0  ");
+            }
         }
         System.out.println();
     }
+    
     }
 }
  
