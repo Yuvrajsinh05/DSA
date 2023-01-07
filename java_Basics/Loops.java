33b5bdcbdb9ffcedcd38886a0d0abd8dfde3c608
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2023-01-07 18:44:36 +0530

    advance patterns

--- java_Basics/Loops.java
+++ java_Basics/Loops.java
@@ -19,7 +19,7 @@ public class Loops {
     do{
         System.out.println(i);
         i++;
-    }while(i<11);
+    }while(i<11); 
  }   
 }
 
--- java_Basics/Patterns.java
+++ java_Basics/Patterns.java
@@ -85,18 +85,55 @@ public class Patterns {
     //     System.out.println();
     // }
     
-    for(int i = 1; i<=n;i++){
-        for(int j=1; j<=i;j++){
-            int sum = i+j;
-            if(sum%2 ==0){
-                System.out.print("1 ");
-            }else{
-                System.out.print("0  ");
-            }
-        }
-        System.out.println();
-    }
-    
+    // for(int i = 1; i<=n;i++){
+    //     for(int j=1; j<=i;j++){
+    //         int sum = i+j;
+    //         if(sum%2 ==0){
+    //             System.out.print("1 ");
+    //         }else{
+    //             System.out.print("0  ");
+    //         }
+    //     }
+    //     System.out.println();
+    // }
+
+
+//  butterfly 
+
+    // upper half
+    // for(int i=1;i<=n;i++){
+    //     // 1st part
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     //spaces
+    //     int spaces = 2 * (n-i);
+    //     for(int j=1 ; j<=spaces ; j++){
+    //         System.out.print(" ");
+    //     }
+    //     //2nd part
+    //     for(int j=1 ; j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println("");
+    // }
+    // for(int i=n;i>=1;i--){
+    //     // 1st part
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     //spaces
+    //     int spaces = 2 * (n-i);
+    //     for(int j=1 ; j<=spaces ; j++){
+    //         System.out.print(" ");
+    //     }
+    //     //2nd part
+    //     for(int j=1 ; j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println("");
+    // }
+
     }
 }
  
