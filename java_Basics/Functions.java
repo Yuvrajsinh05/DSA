ef6dbfcfde1d018b42d2f933b1f4fef33134bb0e
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2023-01-09 19:34:51 +0530

    factorial functions

--- java_Basics/Functions.java
+++ java_Basics/Functions.java
@@ -3,16 +3,47 @@ import java.util.Scanner;
 import javax.swing.plaf.synth.SynthMenuItemUI;
 
 public class Functions {
-    public static int calculateSum(int a ,int b) {
-        int sum = a+b;
-        return sum; 
+    // public static int calculateSum(int a ,int b) {
+    //     int sum = a+b;
+    //     return sum; 
+    // }
+    // public static void main(String args[]){
+    //      Scanner sc =new Scanner(System.in);
+    //     int a =sc.nextInt();
+    //     int b =sc.nextInt();
+
+    //     int sum = calculateSum(a, b);
+    //     System.out.println(sum);
+    // }
+
+    // public static int calculateSum(int a ,int b) {
+    //   return a*b;
+    // }
+    // public static void main(String args[]){
+    //      Scanner sc =new Scanner(System.in);
+    //     int a =sc.nextInt();
+    //     int b =sc.nextInt();
+
+    //     int sum = calculateSum(a, b);
+    //     System.out.println("the product of two num"+  sum);
+    // }
+
+
+    public static void printFactorial(int n) {
+      int factorial = 1;
+      if(n<0){
+        System.out.println("Invalid Number");
+        return;
+      }
+      for(int i=n; i>=1;i--){
+        factorial = factorial * i;
+      }
+      System.out.println(factorial);
+      return;
     }
     public static void main(String args[]){
-         Scanner sc =new Scanner(System.in);
-        int a =sc.nextInt();
-        int b =sc.nextInt();
-
-        int sum = calculateSum(a, b);
-        System.out.println(sum);
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        printFactorial(n);
     }
 }
