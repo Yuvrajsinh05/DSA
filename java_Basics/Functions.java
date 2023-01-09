739dd188b3048e1b860bd7da4620d0f15ff74c25
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2023-01-09 19:03:17 +0530

    function sum

--- java_Basics/Functions.java
+++ java_Basics/Functions.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthMenuItemUI;
+
 public class Functions {
-    public static void printMyName(String name) {
-        System.out.println(name);
-        return;
+    public static int calculateSum(int a ,int b) {
+        int sum = a+b;
+        return sum; 
     }
     public static void main(String args[]){
-        Scanner sc =new Scanner(System.in);
-        String name = sc.next();
+         Scanner sc =new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
 
-        printMyName(name);
+        int sum = calculateSum(a, b);
+        System.out.println(sum);
     }
 }
