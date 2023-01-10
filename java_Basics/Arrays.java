e74be716468bd0e2547847d5326372d469ed2912
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2023-01-10 19:05:33 +0530

    array

--- java_Basics/Arrays.java
+++ java_Basics/Arrays.java
@@ -1,15 +1,16 @@
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String args[]){
         // int[] marks = new int[3];
-        int marks[]= new int[3];
+        Scanner sc =new Scanner(System.in);
+        int siza = sc.nextInt();
+        int marks[]= new int[siza];
         // int marks[]={97,98,95};
         marks[0] = 97; //py
         marks[1] = 98; //chem
         marks[2] = 95; //eng
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
-        for(int i=0;i<3;i++){
+        for(int i=0;i<siza;i++){
             System.out.println(marks[i]);
         }
     }
