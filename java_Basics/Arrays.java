90a5390338376e351ecd6738a820b13ccb326ed1
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2023-01-10 19:01:41 +0530

    array

--- java_Basics/Arrays.java
+++ java_Basics/Arrays.java
@@ -1,6 +1,7 @@
 public class Arrays {
     public static void main(String args[]){
-        int[] marks = new int[3];
+        // int[] marks = new int[3];
+        int marks[]= new int[3];
         marks[0] = 97; //py
         marks[1] = 98; //chem
         marks[2] = 95; //eng
