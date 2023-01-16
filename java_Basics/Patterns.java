01dac8c28084153fce1790d42f7d3522ba8e069c
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2023-01-16 22:47:16 +0530

    patterns

--- java_Basics/Patterns.java
+++ java_Basics/Patterns.java
@@ -158,40 +158,40 @@ public class Patterns {
 
     // palidromic num 
 
-    for(int i=1; i<=n ; i++){
-        //spaces
+    // for(int i=1; i<=n ; i++){
+    //     //spaces
+    //     for(int j=1;j<=n-i;j++){
+    //         System.out.print(" ");
+    //     }
+    //     //1st half num
+    //     for(int j=i;j>=1;j--){
+    //         System.out.print(j);
+    //     }
+    //     //2nd hald num
+    //     for(int j=2;j<=i;j++){
+    //         System.out.print(j);
+    //     } 
+    //     System.out.println();
+    // }
+
+    for(int i =1; i<=n;i++){
         for(int j=1;j<=n-i;j++){
             System.out.print(" ");
         }
-        //1st half num
-        for(int j=i;j>=1;j--){
-            System.out.print(j);
+        for(int j=1; j<=2*i-1;j++){
+            System.out.print("*");
         }
-        //2nd hald num
-        for(int j=2;j<=i;j++){
-            System.out.print(j);
-        } 
         System.out.println();
     }
-
-    // for(int i =1; i<=n;i++){
-    //     for(int j=1;j<=n-i;j++){
-    //         System.out.print(" ");
-    //     }
-    //     for(int j=1; j<=2*i-1;j++){
-    //         System.out.print("*");
-    //     }
-    //     System.out.println();
-    // }
-    // for(int i =n; i>=1;i--){
-    //     for(int j=1;j<=n-i;j++){
-    //         System.out.print(" ");
-    //     }
-    //     for(int j=1; j<=2*i-1;j++){
-    //         System.out.print("*");
-    //     }
-    //     System.out.println();
-    // }
+    for(int i =n; i>=1;i--){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for(int j=1; j<=2*i-1;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
   
     }
 }
