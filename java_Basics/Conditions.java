c90da835a30b5673b47e7d9f696c992e249b9f03
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2023-01-24 22:37:48 +0530

    strike

--- java_Basics/Conditions.java
+++ java_Basics/Conditions.java
@@ -42,20 +42,20 @@ public class Conditions {
     // }
     // }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int button  = sc.nextInt();
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int button  = sc.nextInt();
     
-       switch(button) {
-        case 1 : System.out.println("hello");
-        break;
-        case 2: System.out.println("Namaste");
-        break;
-        case 3: System.out.println("benjaro");
-        break;
-        default :System.out.println("invalid button");
-       }
-    }
+    //    switch(button) {
+    //     case 1 : System.out.println("hello");
+    //     break;
+    //     case 2: System.out.println("Namaste");
+    //     break;
+    //     case 3: System.out.println("benjaro");
+    //     break;
+    //     default :System.out.println("invalid button");
+    //    }
+    // }
 
 
 }
